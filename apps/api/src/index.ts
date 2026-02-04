3d21ba6a7b2e13f7d76f3151e97e62c932dd38f0
Ahmad Hafid (Ocinz) <ahmadhafid28632@gmail.com>
2026-02-04 14:08:22 +0700

    add docker and github workflows

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,15 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { prisma } from "./utils/prisma.js";
 
 const app = new Hono();
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
+});
+app.get("/user", async (c) => {
+  const users = await prisma.user.findMany();
+  return c.json({ users });
 });
 
 serve(
